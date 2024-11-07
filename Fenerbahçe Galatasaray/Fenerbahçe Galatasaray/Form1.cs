b3c9da2e62ef07099f36aaa622e7d2f67d1ae523
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-11-07 17:50:48 +0300

    Add files via upload

---
+++ Fenerbahçe Galatasaray/Fenerbahçe Galatasaray/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fenerbahçe_Galatasaray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rast = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fb, gs;
+            fb = rast.Next(0, 7);
+            gs = rast.Next(0, 7);
+            label1.Text = fb.ToString();
+            label2.Text = gs.ToString();
+        }
+    }
+}
